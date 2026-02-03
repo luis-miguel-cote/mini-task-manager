e2093a584b8eaf7f6721bcaef8ee4e807c3e5d29
Luis Miguel Cote <ing.luismiguelcote@gmail.com>
2026-02-03 17:45:37 -0500

    add router addPost(

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -6,6 +6,12 @@ $router = new Router(false);
 
 $router->setDefaultNamespace('App\Controllers');
 
+
+$router->addPost('/register', [
+    'controller' => 'auth',
+    'action'     => 'register',
+]);
+
 $router->addGet('/test', [
     'controller' => 'test',
     'action'     => 'index',
