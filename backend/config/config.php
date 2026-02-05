9d2fda3dbfd03d0b4af0d4dc4d60049d8565f38d
Luis Miguel Cote <ing.luismiguelcote@gmail.com>
2026-02-05 12:24:37 -0500

    --

--- backend/config/config.php
+++ backend/config/config.php
@@ -13,9 +13,9 @@ return new Config([
     ],
 
     'jwt' => [
-        'secret' => 'f8b9c1e2d4a6F!S@0#QxW3E5R7T9YH2KJ4L6M8ZC',
+        'secret' => 'f8b9c1e2d4a6F!S@0#QxW3E5R7T9YH2KJ4L6M8ZCVBNm',
         'algo'   => 'HS256',
-        'expire' => 3600 // 1 hora
+        'expire' => 3600 // 1 hour
     ],
 
     'app' => [
