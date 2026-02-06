ad82b97ae3314b5a45034d3f3c220c938970c654
Luis Miguel Cote <ing.luismiguelcote@gmail.com>
2026-02-06 12:33:34 -0500

    fix(router): protect tasks route and fix ProtectedRoute import

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,27 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import Tasks from "./features/tasks/Tasks";
+import ProtectedRoute from "./components/ProtectedRoute";
 
-export default function App() {
+function App() {
   return (
     <BrowserRouter>
       <Routes>
+        {/* Rutas públicas */}
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+
+        {/* Rutas protegidas */}
+        <Route element={<ProtectedRoute />}>
+          <Route path="/tasks" element={<Tasks />} />
+        </Route>
+
+        {/* fallback */}
+        <Route path="*" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
 }
+
+export default App;
